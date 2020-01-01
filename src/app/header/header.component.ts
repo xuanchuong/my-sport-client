177be732776062c77ae331e644c62f957ee2a665
xuanchuongdp <xuanchuongdp@gmail.com>
2020-01-01 15:49:03 +0700

    show and hide logout link

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,3 +1,5 @@
+import { Observable } from 'rxjs';
+import { AuthService } from './../auth/auth.service';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,19 +8,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  isLoggedIn: Observable<boolean>; 
   navigatorItems: String[] = [
-    "home", "contact", "logout", "login"
+    "home", "contact"
   ];
 
   selectedItem: String;
     
-  constructor() { }
+  constructor(private authService: AuthService) { }
 
   ngOnInit() {
+    this.isLoggedIn = this.authService.isLoggedIn;
     this.selectedItem = this.navigatorItems[0];
   }
 
   onSelect(item: String): void {
     this.selectedItem = item;
+  }
+  onLogout(){
+    this.authService.logout();
   }
 }
