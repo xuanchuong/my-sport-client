d7ab08bbb6c2fc66841e01e38227997a70b507d2
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-07-17 16:52:51 +0700

    [match] delete the match

--- src/app/match/match-detail/match-detail.component.ts
+++ src/app/match/match-detail/match-detail.component.ts
@@ -1,27 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Match} from "../match";
-import {ActivatedRoute} from "@angular/router";
+import {ActivatedRoute, Router} from "@angular/router";
 import {MatchService} from "../match.service";
+import {FormGroup} from "@angular/forms";
 
 @Component({
-  selector: 'app-match-detail',
-  templateUrl: './match-detail.component.html',
-  styleUrls: ['./match-detail.component.scss']
+	selector: 'app-match-detail',
+	templateUrl: './match-detail.component.html',
+	styleUrls: ['./match-detail.component.scss']
 })
 export class MatchDetailComponent implements OnInit {
-  match: Match;
+	match: Match;
+	deleteForm: FormGroup;
 
-  constructor(
-      private route: ActivatedRoute,
-      private matchService: MatchService,
-  ) { }
+	constructor(
+		private route: ActivatedRoute,
+		private router: Router,
+		private matchService: MatchService,
+	) {
+	}
 
-  ngOnInit() {
-    this.route.paramMap.subscribe(params => {
-      this.matchService.getMatchById(params.get('matchId')).subscribe(match => {
-        this.match = match;
-      })
-    })
-  }
+	ngOnInit() {
+		this.deleteForm = new FormGroup({});
+		this.route.paramMap.subscribe(params => {
+			this.matchService.getMatchById(params.get('matchId')).subscribe(match => {
+				this.match = match;
+			})
+		})
+	}
+
+	onSubmit() {
+		console.log("delete match");
+		this.matchService.delete(this.match.id).then(result => {
+			if (result) {
+				this.router.navigate(['/home']);
+			}
+		})
+	}
 
 }
--- src/app/match/match.service.ts
+++ src/app/match/match.service.ts
@@ -1,48 +1,58 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 import {Observable} from "rxjs";
 import {Match} from "./match";
-import {HttpClient, HttpHeaders} from "@angular/common/http";
+import {HttpClient, HttpHeaders, HttpParams} from "@angular/common/http";
 import {TokenInterceptor} from "../core/auth/token.interceptor";
 import {AuthService} from "../core/auth/auth.service";
+import {map} from "rxjs/operators";
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root'
 })
 export class MatchService {
 
-  createMatchUrl: string;
+	createMatchUrl: string;
+	private matchURL: string;
 
-  constructor(
-    private http: HttpClient,
-    private authService: AuthService
-  ) {
-    TokenInterceptor.init(authService);
-    this.createMatchUrl = "http://localhost:8080/rest/api/v1/match/create";
-  }
+	constructor(
+		private http: HttpClient,
+		private authService: AuthService
+	) {
+		TokenInterceptor.init(authService);
+		this.matchURL = "http://localhost:8080/rest/api/v1/match";
+		this.createMatchUrl = "http://localhost:8080/rest/api/v1/match/create";
+	}
 
-  getAll():Observable<Match[]> {
-    const url = "http://localhost:8080/rest/api/v1/match/all";
-    return this.http.get<Match[]>(url, {});
-  }
+	getAll(): Observable<Match[]> {
+		const url = "http://localhost:8080/rest/api/v1/match/all";
+		return this.http.get<Match[]>(url, {});
+	}
 
-  getMatchById(id: string): Observable<Match> {
-    console.log(id);
-    const url = "http://localhost:8080/rest/api/v1/match/"+id;
-    return this.http.get<Match>(url, {});
-  }
+	getMatchById(id: string): Observable<Match> {
 
-  create(match: Match): Promise<Match> {
-    const data = {
-      "startDate":match.startDate,
-      "location": match.location,
-      "title": match.title,
-      "description": match.description,
-      "numberOfPlayers": match.numberOfPlayers
-    }
-    const body = JSON.stringify(data);
-    const httpOptions = {
-      headers: new HttpHeaders({'Content-Type': 'application/json'})
-    }
-    return this.http.post<Match>(this.createMatchUrl, body, httpOptions).toPromise();
-  }
+		console.log(id);
+		const url = "http://localhost:8080/rest/api/v1/match/" + id;
+		return this.http.get<Match>(url, {});
+	}
+
+	create(match: Match): Promise<Match> {
+		const data = {
+			"startDate": match.startDate,
+			"location": match.location,
+			"title": match.title,
+			"description": match.description,
+			"numberOfPlayers": match.numberOfPlayers
+		}
+		const body = JSON.stringify(data);
+		const httpOptions = {
+			headers: new HttpHeaders({'Content-Type': 'application/json'})
+		}
+		return this.http.post<Match>(this.createMatchUrl, body, httpOptions).toPromise();
+	}
+
+	delete(matchId: number): Promise<boolean> {
+		let params = new HttpParams();
+		params.set("id", matchId.toString());
+		return this.http.delete<boolean>(this.matchURL + "?id=" + matchId).toPromise();
+	}
 }
