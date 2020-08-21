f64a16798dcf926c14ded000c6da911cec425354
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-08-21 17:48:09 +0700

    [#2] move app-matirial to core moduel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,7 +7,6 @@ import {AppRoutingModule} from './app-routing.module';
 import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {LoginComponent} from './login/login.component';
-import {AppMaterialModule} from './app-material/app-material.module';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ReactiveFormsModule} from '@angular/forms';
 import {DashboardComponent} from './dashboard/dashboard.component';
@@ -38,7 +37,6 @@ import {CoreModule} from "./core/core.module";
 		AppRoutingModule,
 		ReactiveFormsModule,
 		BrowserAnimationsModule,
-		AppMaterialModule,
 		MatSnackBarModule,
 		CoreModule
 	],
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,16 +1,26 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {HeaderComponent} from "./layout/header/header.component";
 import {AppFooterComponent} from "./layout/app-footer/app-footer.component";
 import {RouterModule} from "@angular/router";
-
+import {MatToolbarModule} from "@angular/material/toolbar";
+import {MatCardModule} from "@angular/material/card";
+import {MatInputModule} from "@angular/material/input";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatButtonModule} from "@angular/material/button";
 
 
 @NgModule({
-  declarations: [HeaderComponent, AppFooterComponent],
-  imports: [
-    CommonModule, RouterModule
-  ],
-  exports: [HeaderComponent, AppFooterComponent, RouterModule]
+	declarations: [HeaderComponent, AppFooterComponent],
+	imports: [
+		CommonModule, RouterModule,
+		MatToolbarModule,
+		MatCardModule,
+		MatInputModule,
+		MatFormFieldModule,
+		MatButtonModule
+	],
+	exports: [HeaderComponent, AppFooterComponent, RouterModule]
 })
-export class CoreModule { }
+export class CoreModule {
+}
