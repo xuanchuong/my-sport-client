7159d1d8eba594831b8b48db4105a304b5f051c2
chuong.nguyen <chuong.nguyen@mobi.ch>
2020-09-08 13:42:21 +0700

    [#2] add AuthGuard to router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,16 +3,17 @@ import {RouterModule, Routes} from '@angular/router';
 import {DashboardComponent} from './feature/dashboard/dashboard.component';
 import {MatchDetailComponent} from "./feature/match/match-detail/match-detail.component";
 import {MatchCreationComponent} from "./feature/match/match-creation/match-creation.component";
+import {AuthGuard} from "./core/auth/auth.guard";
 
 const routes: Routes = [
     {path: "home", component: DashboardComponent},
     {path: "", component: DashboardComponent},
     {
-        path: "account",
-        loadChildren: () => import('./feature/profile/profile.module').then(m => m.ProfileModule)
+        path: "account", loadChildren: () => import('./feature/profile/profile.module').then(m => m.ProfileModule),
+        canActivate: [AuthGuard]
     },
-    {path: "matches/:matchId", component: MatchDetailComponent},
-    {path: "match/create", component: MatchCreationComponent}
+    {path: "matches/:matchId", component: MatchDetailComponent, canActivate: [AuthGuard]},
+    {path: "match/create", component: MatchCreationComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
